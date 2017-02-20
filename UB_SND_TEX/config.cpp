a1c50d7435d554dc02e15f7ca8ee4ee56a7f95d8
fresqo <rjdesigner@hotmail.com>
2017-02-20 18:57:22 +1100

    Add files via upload

---
+++ UB_SND_TEX/config.cpp
@@ -0,0 +1,16 @@
+#include "BIS_AddonInfo.hpp"
+////////////////////////////////////////////////////////////////////
+//DeRap: Produced from mikero's Dos Tools Dll version 6.03
+//'now' is Tue Jan 31 12:02:03 2017 : 'file' last modified on Sat May 07 23:40:35 2016
+//http://dev-heaven.net/projects/list_files/mikero-pbodll
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+//Class UB_SND_TEX : config.bin{
+class cfgMods
+{
+	author = "76561198087393832";
+	timepacked = "1462664435";
+};
+//};
